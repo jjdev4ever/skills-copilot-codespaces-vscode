c92ab0fa6a1620572bf19ac9a8869938d5e370f9
Jithin J <108987097+jjdev4ever@users.noreply.github.com>
2023-07-19 16:50:38 +0000

    Copilot third commit

---
+++ comment.js
@@ -0,0 +1,10 @@
+// Create web server
+// Create package.json file
+// npm init
+// npm install express --save
+// npm install body-parser --save
+// npm install nodemon --save-dev
+// Create server.js file
+// Create folder "public"
+// Create folder "public/views"
+// Create file
