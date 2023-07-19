cafa9266742f38ce7536bb02b0b16b05000d2ac6
Jithin J <108987097+jjdev4ever@users.noreply.github.com>
2023-07-19 16:46:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember(){
+    var member = {
+        name: "John",
+        age: 23,
+        skills: ["JavaScript", "HTML", "CSS"],
+        calculateNumbers: function(var1, var2){
+            return var1 + var2;
+        }
+    }
+    return member;
+}
